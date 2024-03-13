f3dbd1eaacfe47851a61e75a045b0e612b7c1ae3
HigorAlmeida <higoralmeida7022@gmail.com>
2024-03-13 13:41:44 -0300

    Estudo sobre crud react

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { BrowserRouter, Routes, Link, Route} from 'react-router-dom'
+
+function App() {
+ return (
+   <div className='App'>
+    <h1></h1>
+
+   </div>
+  )
+}
+
+export default App
---
+++ src/componentes/alunos.jsx
@@ -0,0 +1,9 @@
+const Alunos = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Alunos
